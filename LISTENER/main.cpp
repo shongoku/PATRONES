035fbcd91434ae472429d9e82bd490ce3fdaad36
Josemi <canibano_iglesias@hotmail.com>
2025-04-18 23:07:52 +0200

    Insertado el código  de los otros ficheros en el main para ver mejor las otras clases

--- LISTENER/main.cpp
+++ LISTENER/main.cpp
@@ -3,6 +3,38 @@
 #include <iostream>
 
 
+// class EventListener {
+//     public:
+//         virtual void onEvent( int data ) = 0;
+//         virtual ~EventListener() {}
+// };
+
+
+// class EventSource {
+//     std::vector<std::shared_ptr<EventListener>> listeners;
+// public:
+//     void addListener( std::shared_ptr<EventListener> listener) {
+//         listeners.push_back(listener);
+//     }
+// };
+
+
+//     void triggerEvent( int data ) {
+//         for( auto& listener : listeners ) {
+//             listener->onEvent( data );
+//         }
+//     }
+// };
+
+
+// class MyListener : public EventListener {
+//     public:
+//         void onEvent( int data ) override {
+//             std::cout << "Evento recibido. data: " << data << std::endl;
+//         }   
+// };
+
+
 int main() {
     auto source = EventSource();
     auto listener = std::make_shared<MyListener>();
